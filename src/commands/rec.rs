a8dd07d81ebe3e25a7c10cf2a4793096b2610786
Vini <luisvpersy@gmail.com>
2024-07-04 18:31:43 -0300

    atualizando last_reward no rc

--- src/commands/rec.rs
+++ src/commands/rec.rs
@@ -3,13 +3,16 @@ use serenity::prelude::*;
 
 
 use serenity::{all::Message, framework::standard::macros::command};
-use crate::db::update_coins;
+use crate::db::{update_coins, atualize_last_reward};
 
 #[command]
 #[bucket = "req"]
 pub async fn rec(ctx: &Context, msg: &Message, _: Args) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let user_id = msg.author.id.to_string();
+
     let _ = update_coins(&user_id, 100).await.expect("?");
+    let _ = atualize_last_reward(&user_id).await.expect("?");
+
     msg.channel_id.say(&ctx.http, "Você recebeu 100 coins!").await?;
     Ok(())
 }
