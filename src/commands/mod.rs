1014eb563ec665b92c257001b90e43fe833be4f8
Vini <luisvpersy@gmail.com>
2024-07-11 17:15:12 -0300

    ajustando mod de comandos para poise

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,3 +2,14 @@ pub mod par_ou_impar;
 pub mod profile;
 pub mod adm;
 pub mod rec;
+
+use poise::Command;
+
+use crate::{Data, Error};
+
+pub fn get_commands() -> Vec<Command<Data, Error>> {
+    vec![
+        profile::profile(),
+        rec::rec(),
+    ]
+}
